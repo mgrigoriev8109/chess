25c182292bba03c02dc9f59900addb61e64858f7
Mikhail <mikhailgrigoriev92@gmail.com>
2021-12-12 11:56:38 -0500

    Pass test for Rook's #row_movements_left

--- lib/rook.rb
+++ lib/rook.rb
@@ -18,9 +18,9 @@ class Rook
   end
 
 
-  def row_movements(board, rook_location)
+  def row_movements_right(board, rook_location)
     rook_row = rook_location[0]
-    rook_column = rook_location[1]
+    starting_location = rook_location[1]
     row_array = Array.new 
     possible_rook_moves = Array.new
     
@@ -30,13 +30,38 @@ class Rook
       end
     end
 
-    row_array.each_with_index do |value, current_column|
-      if current_column > rook_column && value.is_a?(Rook)
+    row_array.each_with_index do |value, index|
+      if index > starting_location && value.is_a?(Rook)
         break
-      elsif current_column > rook_column
-        possible_rook_moves.push([rook_row, current_column])
+      elsif index > starting_location
+        possible_rook_moves.push([rook_row, index])
       end
     end
+
+    possible_rook_moves
+  end
+
+  def row_movements_left(board, rook_location)
+    rook_row = rook_location[0]
+    reversed_starting_location = 7 - rook_location[1]
+    row_array = Array.new 
+    possible_rook_moves = Array.new
+    
+    board.each_with_index do |board_row, row_index|
+      if rook_row == row_index
+        row_array = board_row.reverse
+      end
+    end
+
+    row_array.each_with_index do |value, index|
+      if index > reversed_starting_location && value.is_a?(Rook)
+        break
+      elsif index > reversed_starting_location
+        reversed_column = 7 - index
+        possible_rook_moves.push([rook_row, reversed_column])
+      end
+    end
+
     possible_rook_moves
   end
 end
--- spec/rook_spec.rb
+++ spec/rook_spec.rb
@@ -5,7 +5,7 @@ require 'player'
 
 describe Rook do
 
-  describe '#row_movements' do
+  describe '#row_movements_right' do
 
     let(:board){Array.new(8) { Array.new(8, " ") } }
     subject(:rook) {described_class.new('white')}
@@ -16,7 +16,7 @@ describe Rook do
       board[0][3] = Rook.new('white')
       board[0][1] = Rook.new('black')
 
-      rook_row_movements = rook.row_movements(board, [0,1])
+      rook_row_movements = rook.row_movements_right(board, [0,1])
       
       expect(rook_row_movements).to eq([[0,2]])
       end
@@ -26,21 +26,29 @@ describe Rook do
       board[0][4] = Rook.new('white')
       board[0][1] = Rook.new('black')
 
-      rook_row_movements = rook.row_movements(board, [0,1])
+      rook_row_movements = rook.row_movements_right(board, [0,1])
       
       expect(rook_row_movements).to eq([[0,2],[0,3]])
       end
-  
-      it "returns possible moves of [[0,0][0,2][0,3]] when stopping at [0][4] and left board edge" do
 
-        board[0][4] = Rook.new('white')
-        board[0][1] = Rook.new('black')
-  
-        rook_row_movements = rook.row_movements(board, [0,1])
-        
-        expect(rook_row_movements).to eq([[0,0],[0,2],[0,3]])
-        end
-    
+    end
+  end
+
+  describe '#row_movements_left' do
+
+    let(:board){Array.new(8) { Array.new(8, " ") } }
+    subject(:rook) {described_class.new('white')}
+    context 'from [0,1]' do
+      it "returns an array of [[0,0]] when stopping at the left board edge" do
+
+      board[0][4] = Rook.new('white')
+      board[0][1] = Rook.new('black')
+
+      rook_row_movements = rook.row_movements_left(board, [0,1])
+      
+      expect(rook_row_movements).to eq([[0,0]])
+      end
+
     end
   end
 end
