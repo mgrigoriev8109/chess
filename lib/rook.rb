3232438808fc1f2a86e820be52129e0496a539c4
Mikhail <mikhailgrigoriev92@gmail.com>
2021-12-12 12:30:04 -0500

    Pass test for Rook's movements_up

--- lib/rook.rb
+++ lib/rook.rb
@@ -17,7 +17,6 @@ class Rook
     symbol
   end
 
-
   def movements_right(board, rook_location)
     starting_row = rook_location[0]
     starting_column = rook_location[1]
@@ -43,22 +42,22 @@ class Rook
 
   def movements_left(board, rook_location)
     starting_row = rook_location[0]
-    reversed_starting_location = 7 - rook_location[1]
+    starting_column = 7 - rook_location[1]
     moves_to_look_through = Array.new 
     possible_moves = Array.new
     
     board.each_with_index do |board_row, row_index|
       if starting_row == row_index
-        moves_to_look_through = board_row.reverse
+        moves_to_look_through = board_row
       end
     end
 
-    moves_to_look_through.each_with_index do |value, index|
-      if index > reversed_starting_location && value.is_a?(Rook)
+    moves_to_look_through.reverse.each_with_index do |value, index|
+      if index > starting_column && value.is_a?(Rook)
         break
-      elsif index > reversed_starting_location
-        reversed_column = 7 - index
-        possible_moves.push([starting_row, reversed_column])
+      elsif index > starting_column
+        index = 7 - index
+        possible_moves.push([starting_row, index])
       end
     end
 
@@ -70,8 +69,6 @@ class Rook
     starting_column = rook_location[1]
     moves_to_look_through = Array.new 
     possible_moves = Array.new
-
-
 
     board.each_with_index do |board_row, row_index|
       board_row.each_with_index do |value, column_index| 
@@ -85,6 +82,32 @@ class Rook
       if index > starting_row && value.is_a?(Rook)
         break
       elsif index > starting_row
+        possible_moves.push([index, starting_column])
+      end
+    end
+
+    possible_moves
+  end
+
+  def movements_up(board, rook_location)
+    starting_row = 7 - rook_location[0]
+    starting_column = rook_location[1]
+    moves_to_look_through = Array.new 
+    possible_moves = Array.new
+
+    board.each_with_index do |board_row, row_index|
+      board_row.each_with_index do |value, column_index| 
+        if starting_column == column_index
+          moves_to_look_through.push(value)
+        end
+      end
+    end
+
+    moves_to_look_through.reverse.each_with_index do |value, index|
+      if index > starting_row && value.is_a?(Rook)
+        break
+      elsif index > starting_row
+        index = index - 7
         possible_moves.push([index, starting_column])
       end
     end
