c88c5bfc9d48270c2fd1b5e58fc9817f72d10c86
Mikhail <mikhailgrigoriev92@gmail.com>
2021-12-13 12:26:36 -0500

    Finish Player's transform input locations

--- lib/player.rb
+++ lib/player.rb
@@ -3,18 +3,31 @@ class Player
   def initialize(color, name)
     @color = color
     @name = name
+    @player_selection
   end
 
-  def get_input
+  def get_input_array
     puts "Players move pieces across the Chessboard using the notation A1B1"
     puts "In the example A1B1, A1 will represent where your piece is located on this turn."
     puts "In the example A1B1, B1 will represent where you want that piece to move."
     puts "Please enter the movement you would like to make:"
-    player_input = gets.chomp
-    player_input
+    @player_selection = gets.chomp.split("")
   end
   
-  def verify_input
-    p get_input
+  def letter_to_numbers(letter)
+    a_to_h_array = ('A'..'H').to_a
+    a_to_h_array.index(letter)
+  end
+
+  def transform_starting_location
+    starting_row = letter_to_numbers(@player_selection[0])
+    starting_column = @player_selection[1].to_i - 1
+    player_starting_selection = [starting_row, starting_column]
+  end
+
+  def transform_ending_location
+    ending_row = letter_to_numbers(@player_selection[2])
+    ending_column = @player_selection[3].to_i - 1
+    player_ending_location = [ending_row, ending_column]
   end
 end
