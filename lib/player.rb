7ecfe14207e303c627e78f788e0ebba23e219716
Mikhail <mikhailgrigoriev92@gmail.com>
2021-12-13 12:01:53 -0500

    Finish Player's #get_input

--- lib/player.rb
+++ lib/player.rb
@@ -1,6 +1,3 @@
-require_relative 'rook'
-require_relative 'current_game'
-
 class Player
 
   def initialize(color, name)
@@ -8,5 +5,16 @@ class Player
     @name = name
   end
 
+  def get_input
+    puts "Players move pieces across the Chessboard using the notation A1B1"
+    puts "In the example A1B1, A1 will represent where your piece is located on this turn."
+    puts "In the example A1B1, B1 will represent where you want that piece to move."
+    puts "Please enter the movement you would like to make:"
+    player_input = gets.chomp
+    player_input
+  end
   
+  def verify_input
+    p get_input
+  end
 end
