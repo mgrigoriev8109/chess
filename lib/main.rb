eaaf43c8094dd2781186c4141cb0a63beecb7930
Mikhail <mikhailgrigoriev92@gmail.com>
2021-12-26 17:21:24 -0500

    Write checklist in main.rb for following steps to take in project

--- lib/main.rb
+++ lib/main.rb
@@ -13,7 +13,25 @@ until current_game.assess_endofround_checkmate(black_player) do
   current_game.play_turn(black_player)
 end
 
-#then continue on to create the Queen (simply copy Rook and Bishop logic)
-#then continue on to create the Knight (follow same movement/attack logic conceptually
-  #as the Rook and Bishop)
-#then continue on to create the Black Pawn and White Pawn
+#modify BlackPawn's methods to match correct direction
+#write tests to check WhitePawn's En Passant attack
+#write tests to check all of BlackPawn's movements and attacks (can duplicate WhitePawn pretty much entirely)
+#write tests for CurrentGame's #verify_enpassant_by_white_pawn and #...by_black_pawn
+#write tests for CurrentGame's #assess_endofround_enpassant
+#play a game from beginning to end to make sure nothing is clearly broken
+#add any possible code needed to play game properly 
+#write code for Pawn promotion
+#test Pawn promotion
+#write code of King Castling
+#test King Castling
+
+#stick check and checkmate into module
+#stick en passant into module
+#stick promotion into module
+#stick castling into module
+
+#implement save/load
+
+#style the game properly
+#go back through Sandi Met'z Testing video and make sure Tests are appropriate
+#go back through POODR and think about possible ways of refactoring
