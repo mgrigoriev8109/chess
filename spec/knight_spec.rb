94379a757905b99ee01e8ba46f60071580ed7417
Mikhail <mikhailgrigoriev92@gmail.com>
2021-12-26 11:19:47 -0500

    Pass tests for Knight's #movements_up_left

--- spec/knight_spec.rb
+++ spec/knight_spec.rb
@@ -20,11 +20,58 @@ describe Knight do
         expect(possible_movement).to eq([[1,4]])
       end
 
+      it "from [3,3] returns an array of [] when there is a piece at [1,4]" do
+
+        board[3][3] = Knight.new('white')
+        board[1][4] = Knight.new('white')
+
+        possible_movement = knight.movements_up_right(board, [3,3])
+        
+        expect(possible_movement).to eq([])
+      end
+
       it "from [0,7] returns an array [] due to no possible movements" do
 
         board[0][7] = Knight.new('white')
 
         possible_movement = knight.movements_up_right(board, [0,7])
+        
+        expect(possible_movement).to eq([])
+      end
+    end
+  end
+
+  describe '#movements_up_left' do
+
+    let(:board){Array.new(8) { Array.new(8, " ") } }
+    subject(:knight) {described_class.new('white')}
+
+    context 'When a knight is looking for possible movement up 2 squares left 1 square' do
+
+      it "from [3,3] returns an array of [[1,2]] when [1,2] is empty" do
+
+        board[3][3] = Knight.new('white')
+
+        possible_movement = knight.movements_up_left(board, [3,3])
+        
+        expect(possible_movement).to eq([[1,2]])
+      end
+
+      it "from [3,3] returns an array of [] when there is a piece at [1,2]" do
+
+        board[3][3] = Knight.new('white')
+        board[1][2] = Knight.new('white')
+
+        possible_movement = knight.movements_up_left(board, [3,3])
+        
+        expect(possible_movement).to eq([])
+      end
+
+      it "from [0,0] returns an array [] due to no possible movements" do
+
+        board[0][0] = Knight.new('white')
+
+        possible_movement = knight.movements_up_left(board, [0,7])
         
         expect(possible_movement).to eq([])
       end
