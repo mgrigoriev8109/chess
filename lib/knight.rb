2291dded854c80df738347f63f7b3fa234f1e51f
Mikhail <mikhailgrigoriev92@gmail.com>
2021-12-26 11:15:11 -0500

    Refactor Knight's #movements_up_right to extract reusable method #find_possible_movement

--- lib/knight.rb
+++ lib/knight.rb
@@ -17,7 +17,27 @@ class Knight < Piece
     symbol
   end 
 
+  def find_possible_movement(board, possible_row, possible_column)
+    possible_move = Array.new
+    board.each_with_index do |board_row, row_index|
+      board_row.each_with_index do |value, column_index| 
+        if row_index == possible_row && column_index == possible_column && value == ' '
+          possible_move.push([row_index, column_index])
+        end
+      end
+    end
+    possible_move
+  end
+
   def movements_up_right(board, coordinates)
+    knight_row = coordinates[0]
+    knight_column = coordinates[1]
+    possible_row = knight_row - 2
+    possible_column = knight_column + 1
+    possible_move = find_possible_movement(board, possible_row, possible_column)
+  end
+
+  def movements_up_left(board, coordinates)
     knight_row = coordinates[0]
     knight_column = coordinates[1]
     possible_row = knight_row - 2
