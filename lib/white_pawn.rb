f531bb3b97fda1553b18784332b63c17ba098ecb
Mikhail <mikhailgrigoriev92@gmail.com>
2021-12-26 12:27:35 -0500

    Pass tests for WhitePawn's #movements

--- lib/white_pawn.rb
+++ lib/white_pawn.rb
@@ -12,7 +12,24 @@ class WhitePawn < Piece
     symbol
   end
 
-  def movement
-  
+  def movements(board, pawn_location)
+    pawn_row = pawn_location[0]
+    pawn_column = pawn_location[1]
+    possible_row = pawn_row - 1
+    possible_moves = Array.new
+
+    board.each_with_index do |board_row, row_index|
+      board_row.each_with_index do |value, column_index| 
+        if row_index == possible_row && column_index == pawn_column && value == ' '
+          possible_moves.push([row_index, column_index])
+        elsif pawn_row == 6 && row_index == 4 && column_index == pawn_column && value == ' '
+          possible_moves.push([row_index, column_index])
+        elsif pawn_row == 6 && row_index == 5 && column_index == pawn_column && value.is_a?(Piece)
+          possible_moves = []
+        end
+      end
+    end
+    possible_moves
   end
+
 end
--- spec/white_pawn_spec.rb
+++ spec/white_pawn_spec.rb
@@ -8,15 +8,36 @@ describe WhitePawn do
     let(:board){Array.new(8) { Array.new(8, " ") } }
     subject(:white_pawn) {described_class.new('white')}
 
-    context 'from [0,1]' do
+    context 'A white pawn looking for possible movements' do
 
-      it "returns an array of [[0,2]] when stopping at [0][3]" do
+      it "returns an array of [[5,0],[4,0]] from its starting location [6,0]" do
 
-        board[0][3] = WhitePawn.new('white')
+        board[6][0] = WhitePawn.new('white')
 
-        possible_movements = white_pawn.movements_right(board, [0,1])
+        possible_movements = white_pawn.movements(board, [6,0])
         
-        expect(possible_movements).to eq([[0,2]])
+        expect(possible_movements).to eq([[4, 0], [5, 0]])
+
+      end
+
+      it "returns an array of [[4,0]] from [5,0]" do
+
+        board[5][0] = WhitePawn.new('white')
+
+        possible_movements = white_pawn.movements(board, [5,0])
+        
+        expect(possible_movements).to eq([[4, 0]])
+
+      end
+
+      it "returns an array of [] from its starting location [6,0] if Piece in front of Pawn" do
+
+        board[6][0] = WhitePawn.new('white')
+        board[5][0] = WhitePawn.new('white')
+
+        possible_movements = white_pawn.movements(board, [6,0])
+        
+        expect(possible_movements).to eq([])
 
       end
     end
