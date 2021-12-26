dac2720f1575459c1c0e3f3cb3ba9086a79756e0
Mikhail <mikhailgrigoriev92@gmail.com>
2021-12-26 11:32:49 -0500

    Pass tests for Knight's remaining movemement methods

--- lib/knight.rb
+++ lib/knight.rb
@@ -53,4 +53,43 @@ class Knight < Piece
     possible_move = find_possible_movement(board, possible_row, possible_column)
   end
 
+  def movements_right_down(board, coordinates)
+    knight_row = coordinates[0]
+    knight_column = coordinates[1]
+    possible_row = knight_row + 1
+    possible_column = knight_column + 2
+    possible_move = find_possible_movement(board, possible_row, possible_column)
+  end
+
+  def movements_down_right(board, coordinates)
+    knight_row = coordinates[0]
+    knight_column = coordinates[1]
+    possible_row = knight_row + 2
+    possible_column = knight_column + 1
+    possible_move = find_possible_movement(board, possible_row, possible_column)
+  end
+
+  def movements_down_left(board, coordinates)
+    knight_row = coordinates[0]
+    knight_column = coordinates[1]
+    possible_row = knight_row + 2
+    possible_column = knight_column - 1
+    possible_move = find_possible_movement(board, possible_row, possible_column)
+  end
+
+  def movements_left_down(board, coordinates)
+    knight_row = coordinates[0]
+    knight_column = coordinates[1]
+    possible_row = knight_row + 1
+    possible_column = knight_column - 2
+    possible_move = find_possible_movement(board, possible_row, possible_column)
+  end
+
+  def movements_left_up(board, coordinates)
+    knight_row = coordinates[0]
+    knight_column = coordinates[1]
+    possible_row = knight_row - 1
+    possible_column = knight_column - 2
+    possible_move = find_possible_movement(board, possible_row, possible_column)
+  end
 end
--- spec/knight_spec.rb
+++ spec/knight_spec.rb
@@ -93,7 +93,96 @@ describe Knight do
         
         expect(possible_movement).to eq([[2,5]])
       end
+    end
+  end
 
+  describe '#movements_right_down' do
+
+    let(:board){Array.new(8) { Array.new(8, " ") } }
+    subject(:knight) {described_class.new('white')}
+
+    context 'When a knight is looking for possible movement 2 squares right and 1 square down' do
+
+      it "from [3,3] returns an array of [[4,5]] when [4,5] is empty" do
+
+        board[3][3] = Knight.new('white')
+
+        possible_movement = knight.movements_right_down(board, [3,3])
+        
+        expect(possible_movement).to eq([[4,5]])
+      end
+    end
+  end
+
+  describe '#movements_down_right' do
+
+    let(:board){Array.new(8) { Array.new(8, " ") } }
+    subject(:knight) {described_class.new('white')}
+
+    context 'When a knight is looking for possible movement 2 squares down and 1 square right' do
+
+      it "from [3,3] returns an array of [[5,4]] when [5,4] is empty" do
+
+        board[3][3] = Knight.new('white')
+
+        possible_movement = knight.movements_down_right(board, [3,3])
+        
+        expect(possible_movement).to eq([[5,4]])
+      end
+    end
+  end
+
+  describe '#movements_down_left' do
+
+    let(:board){Array.new(8) { Array.new(8, " ") } }
+    subject(:knight) {described_class.new('white')}
+
+    context 'When a knight is looking for possible movement 2 squares down and 1 square left' do
+
+      it "from [3,3] returns an array of [[5,2]] when [5,2] is empty" do
+
+        board[3][3] = Knight.new('white')
+
+        possible_movement = knight.movements_down_left(board, [3,3])
+        
+        expect(possible_movement).to eq([[5,2]])
+      end
+    end
+  end
+
+  describe '#movements_left_down' do
+
+    let(:board){Array.new(8) { Array.new(8, " ") } }
+    subject(:knight) {described_class.new('white')}
+
+    context 'When a knight is looking for possible movement 2 squares left and 1 square down' do
+
+      it "from [3,3] returns an array of [[4,1]] when [4,1] is empty" do
+
+        board[3][3] = Knight.new('white')
+
+        possible_movement = knight.movements_left_down(board, [3,3])
+        
+        expect(possible_movement).to eq([[4,1]])
+      end
+    end
+  end
+
+  describe '#movements_left_up' do
+
+    let(:board){Array.new(8) { Array.new(8, " ") } }
+    subject(:knight) {described_class.new('white')}
+
+    context 'When a knight is looking for possible movement 2 squares left and 1 square up' do
+
+      it "from [3,3] returns an array of [[2,1]] when [2,1] is empty" do
+
+        board[3][3] = Knight.new('white')
+
+        possible_movement = knight.movements_left_up(board, [3,3])
+        
+        expect(possible_movement).to eq([[2,1]])
+      end
     end
   end
 end
