fa6f4eb88686d8563b4ecdd34bf3c8a736ac5328
Mikhail <mikhailgrigoriev92@gmail.com>
2021-12-28 13:18:15 -0500

    Pass tests for CurrentGame's #verify_enpassant_by_white_pawn

--- lib/current_game.rb
+++ lib/current_game.rb
@@ -60,41 +60,41 @@ class CurrentGame
     show_display
   end
 
-  def assess_endofround_enpassant(starting_location, ending_location, board)
+  def assess_endofround_enpassant(starting_coordinates, ending_coordinates, board)
     starting_piece = get_piece(starting_location)
-    ending_row = ending_location[0]
-    ending_column = ending_location[1]
+    starting_row = starting_coordinates[0]
+    ending_row = ending_coordinates[0]
 
-    if starting_piece.is_a?(WhitePawn)
-      verify_enpassant_by_black_pawn(ending_location)
-    elsif starting_piece.is_a?(BlackPawn)
-      verify_enpassant_by_white_pawn(ending_location)
+    if starting_piece.is_a?(WhitePawn) && starting_row == 6 && ending_row == 4 
+      verify_enpassant_by_black_pawn(ending_location, board)
+    elsif starting_piece.is_a?(BlackPawn) && starting_row == 1 && ending_row == 3
+      verify_enpassant_by_white_pawn(ending_location, board)
     end
 
   end
 
-  def verify_enpassant_by_white_pawn(ending_location)
+  def verify_enpassant_by_white_pawn(ending_location, board)
     black_pawn_column = ending_location[1]
-    white_pawn_row = ending_location[0] - 1
-    white_pawn_columns = [(ending_location[1] - 1), (ending_location[1] + 1)]
+    white_pawn_row = ending_location[0]
+    white_pawn_columns = [(black_pawn_column - 1), (black_pawn_column + 1)]
 
     board.each_with_index do |row, row_index|
       row.each_with_index do |cell, column_index|
-        if white_pawn_row == row_index && white_pawn_columns.include?(column_index) && cell.is_a?(WhitePawn)
+        if white_pawn_row == 3 && white_pawn_columns.include?(column_index) && cell.is_a?(WhitePawn)
           cell.can_en_passant_column = black_pawn_column
         end
       end
     end
   end
 
-  def verify_enpassant_by_black_pawn(ending_location)
+  def verify_enpassant_by_black_pawn(starting_location, ending_location, board)
     white_pawn_column = ending_location[1]
-    black_pawn_row = ending_location[0] + 1
-    black_pawn_columns = [(ending_location[1] - 1), (ending_location[1] + 1)]
+    black_pawn_row = ending_location[0]
+    black_pawn_columns = [(white_pawn_column - 1), (white_pawn_column + 1)]
 
     board.each_with_index do |row, row_index|
       row.each_with_index do |cell, column_index|
-        if black_pawn_row == row_index && black_pawn_columns.include?(column_index) && cell.is_a?(BlackPawn)
+        if black_pawn_row == 4 && black_pawn_columns.include?(column_index) && cell.is_a?(BlackPawn)
           cell.can_en_passant_column = white_pawn_column
         end
       end
--- spec/current_game_spec.rb
+++ spec/current_game_spec.rb
@@ -384,4 +384,38 @@ describe CurrentGame do
       end
     end
   end
+
+  describe '#verify_enpassant_by_white_pawn' do
+
+    subject(:current_game) {described_class.new}
+
+    context 'When verifying if a White Pawn can perform En Passant attack on Black Pawn' do
+
+      it "returns the White Pawn's @can_en_passant_column to be the adjacent Black Pawn's column when En Passant possible" do
+        
+        current_game.board[1][1] = BlackPawn.new('black')
+        current_game.board[3][0] = WhitePawn.new('white')
+        white_pawn = current_game.board[3][0]
+        black_pawn_ending_location = [3,1]
+
+        current_game.verify_enpassant_by_white_pawn(black_pawn_ending_location, current_game.board)
+
+
+        expect(white_pawn.can_en_passant_column).to eq(1)
+      end
+
+      it "returns the White Pawn's @can_en_passant_column to be nil when White Pawn not adjescent, and En Passant not possible" do
+        
+        current_game.board[1][1] = BlackPawn.new('black')
+        current_game.board[3][3] = WhitePawn.new('white')
+        white_pawn = current_game.board[3][3]
+        black_pawn_ending_location = [3,1]
+
+        current_game.verify_enpassant_by_white_pawn(black_pawn_ending_location, current_game.board)
+
+
+        expect(white_pawn.can_en_passant_column).to eq(nil)
+      end
+    end
+  end
 end
