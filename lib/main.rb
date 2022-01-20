3399d73f8c15fdc59b9be011010238ed3b9a5e94
Mikhail <mikhailgrigoriev92@gmail.com>
2022-01-20 11:33:38 -0500

    Refactor Driver Script's logic for playing a full game

--- lib/main.rb
+++ lib/main.rb
@@ -2,8 +2,8 @@ require_relative 'current_game'
 require_relative 'player'
 
   current_game = CurrentGame.new
-  current_game.introduction
   current_game.populate_gameboard
+  puts "Welcome to a CLI game of Chess!"
   puts "What will be the name of the White Player? If this player is to be a computer, type the name Computer"
   white_player = current_game.create_player("white")
   puts "What will be the name of the Black Player? If this player is to be a computer, type the name Computer"
@@ -11,21 +11,37 @@ require_relative 'player'
   current_game.show_display
 
 until current_game.assess_endofgame('black', current_game.board) do 
+  movement = []
 
   if white_player.name == 'Computer'
-    current_game.computer_turn(white_player)
+    movement = current_game.determine_computer_movement(white_player.color, current_game.board
   else
-    current_game.human_turn(white_player)
+    movement = current_game.get_input(white_player)
   end
+  current_game.play_turn(movement)
 
   break if current_game.assess_endofgame('white', current_game.board)
 
   if black_player.name == 'Computer'
-    current_game.computer_turn(black_player)
+    movement = current_game.determine_computer_movement(black_player.color, current_game.board)
   else
-    current_game.human_turn(black_player)
+    movement = current_game.get_input(black_player)
   end
+  current_game.play_turn(movement)
+
 end
+
+#refactor #human_turn and #computer_turn to only take movement as argument
+  #-create seperate #get_input function
+
+#create integration tests for #human_turn to make sure En Passant works with multiple #human_turns
+  #-currently En Passant never resets, all Pawns should reset at the end of the turn
+
+#do the same to test to fix Castling
+  #-currently White King can't castle left
+
+#do the same test to fix Checkmate, 
+  #-currently White King in checkmate even if a piece can get in the way and disrupt that checkmate
 
 #implement save/load
 #style chess properly
